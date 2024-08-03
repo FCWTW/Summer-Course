e715d20825a55c691df4e19fb9d0943fc5477d98
user <user@SummerCourse.myguest.virtualbox.org>
2024-08-03 11:40:15 +0800

    little change

--- Week 1/HW2/Problem_2_sol.cpp
+++ Week 1/HW2/Problem_2_sol.cpp
@@ -5,48 +5,48 @@ using namespace std;
 
 class Problem_2 {
     public:
-		void Problem_2_sol(string s) {
-			int size = s.size();
-			int start = 0;
-			int length = 0;
+        void Problem_2_sol(string s) {
+            int size = s.size();
+            int start = 0;
+            int length = 0;
 
-			for(int i=0; i<size; i++) {
-				for(int j=i; j<size; j++) {
-					if(check(s, i, j)) {
-					if(j-i+1 > length) {
-						// update length of substring
-						length = j-i+1;
-						start = i;
-						}
-					}
-				}
-			}
+            for(int i=0; i<size; i++) {
+                for(int j=i; j<size; j++) {
+                    if(check(s, i, j)) {
+                        if(j-i+1 > length) {
+                            // update length of substring
+                            length = j-i+1;
+                            start = i;
+                        }
+                    }
+                }
+            }
 
-			cout << "Output: \"";
-			for(int i=start; i<(start+length); i++)
-			cout << s[i];
-			cout << "\"" << endl;
-		}
+            cout << "Output: \"";
+            for(int i=start; i<(start+length); i++)
+                cout << s[i];
+            cout << "\"" << endl;
+        }
 
-	private:
-		bool check(string &s, int index1, int index2) {
-			while(index1 < index2) {
-				if(s[index1] != s[index2])
-					return false;
-				index1++;
-				index2--;
-			}
+    private:
+        bool check(string &s, int index1, int index2) {
+            while(index1 < index2) {
+                if(s[index1] != s[index2])
+                        return false;
+                    index1++;
+                    index2--;
+                }
 
-			return true;
-		}
+            return true;
+        }
 };
 
 //	test
 int main() {
-	Problem_2 test;
-	string s = "babad";
+    Problem_2 test;
+    string s = "babad";
 
-	test.Problem_2_sol(s);
+    test.Problem_2_sol(s);
 
-	return 0;
+    return 0;
 }
