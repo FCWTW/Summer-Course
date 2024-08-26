93a81a36a5e04c4ac1d91efc60ffbe44b8789d34
user <user@SummerCourse.myguest.virtualbox.org>
2024-08-26 15:09:29 +0800

    update hw1

--- Week 4/HW1/README.md
+++ Week 4/HW1/README.md
@@ -1,2 +1,53 @@
 # OpenCV practice: Active Contour
 
+## How to run:
+> ### 1. Prerequisites:
+> * install cmake, pkg-config, lib* with following commands
+> ```bash
+> sudo apt-get install gcc g++ cmake pkg-config build-essential
+> sudo apt-get install libgtk2.0-dev libavcodec-dev libavformat-dev libtiff5-dev libswscale-dev
+> ```
+>
+> * download opencv from github
+> ```bash
+> git clone https://github.com/opencv/opencv.git
+> ```
+>
+> * compile and install opencv
+> ```bash
+> cd opencv 
+> mkdir build
+> cd build
+> cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
+> make -j$(nproc)
+> sudo make install
+> ```
+>
+> * update system library cache
+> ```bash
+> echo "/usr/local/lib" | sudo tee /etc/ld.so.conf.d/opencv.conf
+> sudo ldconfig
+> ```
+>
+> ### 2. Compilation: use the following command in terminal
+> ```bash
+> g++ hw1.cpp -o hw1 `pkg-config --cflags --libs opencv4`
+> ```
+>
+> ### 3. Execution: use the following command in terminal
+> ```bash
+> ./hw1
+> ```
+>
+## Example
+
+> <details>
+>  <summary>參考資料</summary>
+>    1. <a href="https://hackmd.io/@xpg109/H1wZiRUNF">Linux環境下安裝OpenCV 4.5.3 (C++)</a><br>
+>    2. <a href="https://www.cnblogs.com/chenzhen0530/p/14660498.html">OpenCV-C++ Sobel算子使用</a><br>
+>    3. <a href="https://stackoverflow.com/questions/21079758/opencv-c-drawing-on-image">Opencv c++ drawing on image</a><br>
+>    4. <a href="https://www.cs.ait.ac.th/~mdailey/cvreadings/Kass-Snakes.pdf">Snakes: Active Contour Models</a><br>
+>    5. <a href="https://github.com/webzhuce07/Digital-Image-Processing/blob/master/Segment/CvSnakeImage/snakeimage.cpp#L451">用OpenCV3.x重新實現cvSnakeImage函數</a><br>
+>    6. <a href="https://github.com/bosley/Active-Contours/blob/master/src/Vision/Algorithms/customsnake.cpp">custom snake algorithm</a><br>
+> </details>
+
---
+++ Week 4/HW1/hw1.cpp
@@ -0,0 +1,221 @@
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+#define MAX_ITERATION 500
+
+vector<Point> points;
+bool converged = false;
+Mat original_img, imgCopy;
+
+
+//  draw line between points
+void drawPoints(Mat img) {
+    for(int i=0; i<points.size(); i++) {
+        int j = (i+1)%points.size();
+        line(img, points[i], points[j], Scalar(255, 0, 0), 2);
+    }
+    cout << "draw"<< endl;
+    imshow("Active Contour", img);
+    waitKey(500);
+}
+
+
+//  active contour
+void ACTIVE_CONTOUR(Mat &dx, Mat &dy) {
+    //  set search region
+    Size win(7, 7); 
+
+    //  numbers of points in search region
+    int neighbors = win.height * win.width;
+    
+    float alpha = 0.4;
+    float beta = 0.6;
+    float gamma = 0.8;
+
+    //  the center of search region
+    int centerx = win.width >> 1;
+    int centery = win.height >> 1;
+
+    float *Econt = new float[neighbors];
+	float *Ecurv = new float[neighbors];
+	float *Eimg = new float[neighbors];
+	float *E = new float[neighbors];
+
+    int movecount = 0;
+    float averagedistance = 0;
+    float tmp;
+    int offsetx, offsety;
+
+    //  calculate average distance between points
+    for (size_t i = 0; i < points.size(); ++i) {
+        Point diff = (i > 0) ? (points[i - 1] - points[i]) : (points.back() - points[i]);
+        averagedistance += norm(diff);
+    }
+    averagedistance /= points.size();
+
+    //  calculate energies
+    for (size_t i = 0; i < points.size(); i++) {
+        float maxEcont = 0.0f;
+        float minEcont = 2.e+38f;
+        float maxEcurv = 0.0f;
+        float minEcurv = 2.e+38f;
+        float maxEimg = 0.0f;
+        float minEimg = 2.e+38f;
+        
+        //  adjust the search range to avoid exceeding the image range
+        int left = min(points[i].x, centerx);
+        int right = min(original_img.cols - 1 - points[i].x, centerx);
+        int upper = min(points[i].y, centery);
+        int bottom = min(original_img.rows - 1 - points[i].y, centery);
+
+        //  evaluate energy for each pixel within the search region
+        for (int j = -upper; j <= bottom; j++) {
+            for (int k = -left; k <= right; k++) {
+                int nx = points[i].x + k;
+                int ny = points[i].y + j;
+
+                //  calculate E cout
+                Point prev_diff = (i > 0) ? (points[i - 1] - Point(nx, ny)) : (points.back() - Point(nx, ny));
+                float E_cont = abs(averagedistance - norm(prev_diff));
+                Econt[(j + centery) * win.width + k + centerx] = E_cont;
+                maxEcont = max(maxEcont, E_cont);
+                minEcont = min(minEcont, E_cont);
+
+                //  calculate E curv
+                Point next_diff;
+                if(i == 0)
+                    next_diff = points[points.size()-1] - 2*Point(nx, ny) + points[i+1];
+                else if(i == points.size()-1)
+                    next_diff = points[i-1] - 2*Point(nx, ny) + points[0];
+                else
+                    next_diff = points[i-1] - 2*Point(nx, ny) + points[i+1];
+                float E_curv = norm(next_diff - 2 * prev_diff);
+                Ecurv[(j + centery) * win.width + k + centerx] = E_curv;
+                maxEcurv = max(maxEcurv, E_curv);
+                minEcurv = min(minEcurv, E_curv);
+
+                //  calculate E img
+                float gradient_x = dx.at<float>(ny, nx);
+                float gradient_y = dy.at<float>(ny, nx);
+                float E_img = pow(gradient_x, 2) + pow(gradient_y, 2);
+                Eimg[(j + centery) * win.width + k + centerx] = E_img;
+                maxEimg = max(maxEimg, E_img);
+                minEimg = min(minEimg, E_img);
+            }
+        }
+
+        //  normalize E cout
+        tmp = maxEcont - minEcont;
+        tmp = (tmp == 0) ? 0.0f : (1.0/tmp);
+        for (int j = 0; j < neighbors; j++) {
+            Econt[j] = (Econt[j] - minEcont) * tmp;
+        }
+
+        //  normalize E curv
+        tmp = maxEcurv - minEcurv;
+        tmp = (tmp == 0) ? 0.0f : (1.0/tmp);
+        for (int j = 0; j < neighbors; j++) {
+            Ecurv[j] = (Ecurv[j] - minEcurv) * tmp;
+        }
+
+        //  normalize E img
+        tmp = maxEimg - minEimg;
+        tmp = (tmp == 0) ? 0.0f : (1.0/tmp);
+        for (int j = 0; j < neighbors; j++) {
+            Eimg[j] = (minEimg - Eimg[j]) * tmp;
+        }
+
+        //  calculate total energy
+        for (int j = 0; j < neighbors; j++) {
+            E[j] = alpha * Econt[j] + beta * Ecurv[j] + gamma * Eimg[j];
+        }
+
+        //  find best move
+        float min_total_energy = 2.e+38f;
+        Point best_move = points[i];
+        for (int j = -upper; j <= bottom; j++)
+			for (int k = -left; k <= right; k++)
+					if (E[(j + centery) * win.width + k + centerx] < min_total_energy) {
+						min_total_energy = E[(j + centery) * win.width + k + centerx];
+						offsetx = k;
+						offsety = j;
+                    }
+
+        //  update point position 
+		if(offsetx || offsety) {
+            points[i] += Point(offsetx, offsety);
+            movecount++;
+        }
+    }
+
+    converged = (movecount  == 0) ? true : false;
+
+    //  draw new lines
+    cout << "Update points" << endl;
+    imgCopy = original_img.clone();
+    drawPoints(imgCopy);
+
+    delete[] Econt;
+	delete[] Ecurv;
+	delete[] Eimg;
+	delete[] E;
+}
+
+
+//  draw points when mouse click
+static void onMouse(int event, int x, int y, int, void* imgptr){
+    if (event == EVENT_LBUTTONDOWN) {
+        Mat &img = *(Mat*)imgptr;
+        Point pt = Point(x, y);
+        points.push_back(pt);
+        circle(img, pt, 3, Scalar(0, 255, 0), -1);
+        imshow("Active Contour", img);
+    }
+}
+
+
+int main() {
+    //  Read the image
+    original_img = imread("img.jpg", IMREAD_COLOR);
+    imgCopy = original_img.clone();
+
+    if (original_img.empty()) {
+        cout << "Could not open or find the image!" << endl;
+        return -1;
+    }
+
+    //  Convert the image to Grayscale
+    Mat gray_img;
+    cvtColor(original_img, gray_img, COLOR_BGR2GRAY);
+    
+    //  Denoising with Gaussian blur
+    Mat blurred_img;
+    GaussianBlur(gray_img, blurred_img, Size(5, 5), 2);
+    
+    //  Calculate the gradient by using Sobel
+    Mat gradX, gradY;
+    Sobel(blurred_img, gradX, CV_32F, 1, 0, 3);
+    Sobel(blurred_img, gradY, CV_32F, 0, 1, 3);
+    
+    //  innitialize position of points
+    namedWindow("Active Contour", 0);
+    resizeWindow("Active Contour", 700, 700);
+    imshow("Active Contour", imgCopy);
+    setMouseCallback("Active Contour", onMouse, &imgCopy);
+    cout << "Enter '0' to continue..." << endl;
+    waitKey(0);
+    drawPoints(imgCopy);
+
+    for (int i = 0; i < MAX_ITERATION; ++i) {
+        ACTIVE_CONTOUR(gradX, gradY);
+        if(converged) break;
+    }
+    
+    cout << "Finish !" << endl << "Enter '0' to exit" << endl;
+    waitKey(0);
+    return 0;
+}
